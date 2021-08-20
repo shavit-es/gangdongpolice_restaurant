d3d11320f270f46a0e8ba3b61d31b6e198ff3528
shavit-es <shavit0423@naver.com>
2021-08-20 04:33:08 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <!-- <link rel="canonical" href="https://brandtest.site/"> -->
+        <meta name="robots" content="index,follow" />
+        <title>강동경찰서 구내식당</title>
+        <meta name="description" content="강동경찰서 구내식당입니다." />
+        <meta property="og:title" content="강동경찰서 구내식당" />
+        <meta property="og:description" content="강동경찰서 구내식당" />
+        <meta property="og:image" content="images/android-chrome-512x512.png" />
+        <meta property="og:url" content="https://brandtest.site/" />
+        <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
+        <meta name="NaverBot" content="All" />
+        <meta name="NaverBot" content="index,follow" />
+        <meta name="Yeti" content="All" />
+        <meta name="Yeti" content="index,follow" />
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <a class="toptitle">강동경찰서 구내식당</a>
+        <?php
+		echo "Contents here";
+		?>
+    </body>
+</html>
