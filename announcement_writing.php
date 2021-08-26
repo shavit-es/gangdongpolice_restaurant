e14c34b29074b84872f51a99964fa5ca461a5b1f
shavit-es <shavit0423@naver.com>
2021-08-26 06:29:56 +0000

    쓰기 부분에 비밀번호 구현

--- announcement_writing.php
+++ announcement_writing.php
@@ -1,6 +1,15 @@
 <?php
 include "commonpart.php";
 $conn = mysqli_connect("localhost", "shavit0423", "hyun0430!@my", "announcement");
+$sql = 'SELECT password from password;';
+$result = mysqli_query($conn,$sql);
+$adminpass = mysqli_fetch_array($result)['password'];
+$passwordinputstr='<form method="POST"><div class="container p-4 text-center">
+<br>관리자만이 글을 작성할 수 있습니다. <br>비밀번호를 입력해주세요.<div class="input-group mt-3 mb-1">
+  <span class="input-group-text" id="basic-addon1">비밀번호</span>
+  <input type="password" name="password" class="form-control" aria-label="Username" aria-describedby="basic-addon1"></div><p class="text-end mt-3">
+<input class="btn btn-primary btn-sm" type="submit" value="작성"/></p></div>
+<p class="text-end mt-3"></p></div></form>';
 ?>
 <script>
 function checker(){
@@ -17,7 +26,12 @@ function checker(){
 	<?=$headpart?>
 	<body>
 		<?=$navbar?>
-		<div class="container p-4">
+		<?=$passwordinputstr?>
+	<?php
+	$i = 0;
+	$inputpass= $_POST['password'];
+	if (password_verify($inputpass, $adminpass)){
+		echo '<div class="container p-4">
 			<form name="fo" action="announce_create_process.php" method="POST" onsubmit="return checker();">
 				<div class="input-group input-group-sm">
 					<span class="input-group-text" id="inputGroup-sizing-default">제목</span>
@@ -43,6 +57,15 @@ function checker(){
 					<input class="btn btn-primary btn-sm" type="submit" value="작성"/>
 				</p>
 			</form>
-		</div>
+		</div>';
+	}else{
+		$i +=0;
+		if($i === 5){
+			header('Location: announcement.php');
+		}
+		echo '<p class=text-center>비밀번호를 '.$i.'회 틀렸습니다. <br>5회 틀릴 시 뒤로 돌아갑니다.';
+	}
+	?>
+		
 	</body>
 </html>
--- commonpart.php
+++ commonpart.php
@@ -22,5 +22,4 @@ $headpart='
 	/>
 	<link rel="stylesheet" type="text/css" href="/css/style.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />';
-
 ?>
