6d3476041dbe066ebbb87a9aa7439df9698a8d21
shavit-es <shavit0423@naver.com>
2021-08-27 23:06:28 +0000

    favicon 및 ogimage, index 사진 추가 및 적용

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ include "commonpart.php";
 	</head>
 	<body>
 		<?=$navbar?>
+		<img class="imgsize rounded mx-auto d-block m-2"src='images/android-chrome-512x512.png'/>
+
 		<script
 			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
 			integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
