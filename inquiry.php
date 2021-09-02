904575e25a4f67e9c9d817aae6e7596097cc84bf
shavit-es <shavit0423@naver.com>
2021-09-02 07:29:28 +0000

    이틀치 조사 구현

--- inquiry.php
+++ inquiry.php
@@ -8,26 +8,37 @@
 	crossorigin = 'anonymous';
 </script>
 <?php
-if(!isset($_COOKIE["breakfast"])){ //첫 세팅
-	setcookie('breakfast', 0, time()+86400, '/');
+if(!isset($_COOKIE["breakfasttoday"])){ //첫 세팅
+	setcookie('breakfasttoday', 0, time()+86400, '/');
 }
-if(!isset($_COOKIE["lunch"])){
-	setcookie('lunch', 0, time()+86400, '/');
+if(!isset($_COOKIE["lunchtoday"])){
+	setcookie('lunchtoday', 0, time()+86400, '/');
 }
-if(!isset($_COOKIE["dinner"])){
-	setcookie('dinner', 0, time()+86400, '/');
+if(!isset($_COOKIE["dinnertoday"])){
+	setcookie('dinnertoday', 0, time()+86400, '/');
+}
+
+if(!isset($_COOKIE["breakfasttomorrow"])){ //첫 세팅
+	setcookie('breakfasttoday', 0, time()+86400, '/');
+}
+if(!isset($_COOKIE["lunchtomorrow"])){
+	setcookie('lunchtoday', 0, time()+86400, '/');
+}
+if(!isset($_COOKIE["dinnertomorrow"])){
+	setcookie('dinnertoday', 0, time()+86400, '/');
 }
 include "commonpart.php";
 $conn = mysqli_connect("localhost", "shavit0423", "hyun0430!@my", "numeat");
-$sqlloadb="SELECT num_eat from numeat WHERE meal='breakfast';";
-$resultb=mysqli_query($conn, $sqlloadb);
-$rowb=mysqli_fetch_array($resultb)['num_eat'];
-$sqlloadl="SELECT num_eat from numeat WHERE meal='lunch';";
-$resultl=mysqli_query($conn, $sqlloadl);
-$rowl=mysqli_fetch_array($resultl)['num_eat'];
-$sqlloadd="SELECT num_eat from numeat WHERE meal='dinner';";
-$resultd=mysqli_query($conn, $sqlloadd);
-$rowd=mysqli_fetch_array($resultd)['num_eat'];
+$sqli = "SELECT * FROM inquiry order by id desc LIMIT 2";
+$resulti=mysqli_query($conn, $sqli);
+$meals = mysqli_fetch_array($resulti);
+$resultbtomorrow=htmlspecialchars($meals['breakfast']);
+$resultltomorrow=htmlspecialchars($meals['lunch']);
+$resultdtomorrow=htmlspecialchars($meals['dinner']);
+$meals = mysqli_fetch_array($resulti);
+$resultbtoday=htmlspecialchars($meals['breakfast']);
+$resultltoday=htmlspecialchars($meals['lunch']);
+$resultdtoday=htmlspecialchars($meals['dinner']);
 date_default_timezone_set('Asia/Seoul');
 $todaytime = strtotime("Now");
 $today=date("m/d", $todaytime);
@@ -41,57 +52,73 @@ $tomorrow = date("m/d", $tommorowtime);
 	</head>
 	<body>
 		<?=$navbar?>
-		<!-- <p class="text-center choose">
+		<p class="text-center choose">
 			금일(<?=$today?>) 식사하실 끼니를 선택해주세요.
 		</p>
 		<div class="d-flex justify-content-around p-3 m-3 bd-highlight border-bottom">
 			<form action="inquiry_process.php" method="POST">
-				<input type="submit" value="조식" name="조식" class="inquiry_btn btn btn-sm" id="breakfast_btn"/>
-				<p class="text-center"><?=$rowb?></p>
+				<input type="submit" value="조식" name="조식오늘" class="inquiry_btn btn btn-sm" id="breakfasttoday_btn"/>
+				<p class="text-center"><?=$resultbtoday?></p>
 			</form>
 			<form action="inquiry_process.php" method="POST">
-				<input type="submit" value="중식" name="중식" class="inquiry_btn btn btn-sm" id="lunch_btn"/>
-				<p class="text-center"><?=$rowl?></p>
+				<input type="submit" value="중식" name="중식오늘" class="inquiry_btn btn btn-sm" id="lunchtoday_btn"/>
+				<p class="text-center"><?=$resultltoday?></p>
 			</form>
 			<form action="inquiry_process.php" method="POST">
-				<input type="submit" value="석식" name="석식" class="inquiry_btn btn btn-sm" id="dinner_btn"/>
-				<p class="text-center"><?=$rowd?></p>
+				<input type="submit" value="석식" name="석식오늘" class="inquiry_btn btn btn-sm" id="dinnertoday_btn"/>
+				<p class="text-center"><?=$resultdtoday?></p>
 			</form>
 		</div>
-		<br> -->
+		<br>
 		<p class="text-center choose">
 			내일(<?=$tomorrow?>) 식사하실 끼니를 선택해주세요.
 		</p>
 		<div class="d-flex justify-content-around p-3 m-3 bd-highlight border-bottom">
 			<form action="inquiry_process.php" method="POST">
-				<input type="submit" value="조식" name="조식" class="inquiry_btn btn btn-sm" id="breakfast_btn"/>
-				<p class="text-center"><?=$rowb?></p>
+				<input type="submit" value="조식" name="조식내일" class="inquiry_btn btn btn-sm" id="breakfasttomorrow_btn"/>
+				<p class="text-center"><?=$resultbtomorrow?></p>
 			</form>
 			<form action="inquiry_process.php" method="POST">
-				<input type="submit" value="중식" name="중식" class="inquiry_btn btn btn-sm" id="lunch_btn"/>
-				<p class="text-center"><?=$rowl?></p>
+				<input type="submit" value="중식" name="중식내일" class="inquiry_btn btn btn-sm" id="lunchtomorrow_btn"/>
+				<p class="text-center"><?=$resultltomorrow?></p>
 			</form>
 			<form action="inquiry_process.php" method="POST">
-				<input type="submit" value="석식" name="석식" class="inquiry_btn btn btn-sm" id="dinner_btn"/>
-				<p class="text-center"><?=$rowd?></p>
+				<input type="submit" value="석식" name="석식내일" class="inquiry_btn btn btn-sm" id="dinnertomorrow_btn"/>
+				<p class="text-center"><?=$resultdtomorrow?></p>
 			</form>
 		</div>
 <?php
-//버튼 표시
-if(isset($_COOKIE["breakfast"]) and $_COOKIE["breakfast"]==1){
-	echo '<script>$("#breakfast_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
+//버튼 표시 오늘
+if(isset($_COOKIE["breakfasttoday"]) and $_COOKIE["breakfasttoday"]==1){
+	echo '<script>$("#breakfasttoday_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
 }else{
-	echo '<script>$("#breakfast_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
+	echo '<script>$("#breakfasttoday_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
 }
-if(isset($_COOKIE["lunch"]) and $_COOKIE["lunch"]==1){
-	echo '<script>$("#lunch_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
+if(isset($_COOKIE["lunchtoday"]) and $_COOKIE["lunchtoday"]==1){
+	echo '<script>$("#lunchtoday_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
 }else{
-	echo '<script>$("#lunch_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
+	echo '<script>$("#lunchtoday_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
 }
-if(isset($_COOKIE["dinner"]) and $_COOKIE["dinner"]==1){
-	echo '<script>$("#dinner_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
+if(isset($_COOKIE["dinnertoday"]) and $_COOKIE["dinnertoday"]==1){
+	echo '<script>$("#dinnertoday_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
 }else{
-	echo '<script>$("#dinner_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
+	echo '<script>$("#dinnertoday_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
+}
+
+if(isset($_COOKIE["breakfasttomorrow"]) and $_COOKIE["breakfasttomorrow"]==1){
+	echo '<script>$("#breakfasttomorrow_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
+}else{
+	echo '<script>$("#breakfasttomorrow_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
+}
+if(isset($_COOKIE["lunchtomorrow"]) and $_COOKIE["lunchtomorrow"]==1){
+	echo '<script>$("#lunchtomorrow_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
+}else{
+	echo '<script>$("#lunchtomorrow_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
+}
+if(isset($_COOKIE["dinnertomorrow"]) and $_COOKIE["dinnertomorrow"]==1){
+	echo '<script>$("#dinnertomorrow_btn").removeClass("btn-primary").addClass("btn-secondary");</script>';
+}else{
+	echo '<script>$("#dinnertomorrow_btn").removeClass("btn-secondary").addClass("btn-primary");</script>';
 }
 ?>
 	</body>
--- inquiry_process.php
+++ inquiry_process.php
@@ -2,42 +2,82 @@
 include "commonpart.php";
 $conn = mysqli_connect("localhost", "shavit0423", "hyun0430!@my", "numeat");
 //안눌러져 있는 상태에서 눌렀을 때
-if(($_COOKIE["breakfast"]==0) and (!empty($_POST['조식'])) and ( $_POST['조식'] == '조식' )) {
-	$sql1 = "UPDATE numeat SET num_eat=(num_eat+1) WHERE meal='breakfast';";
+if(($_COOKIE["breakfasttomorrow"]==0) and (!empty($_POST['조식내일'])) and ( $_POST['조식내일'] == '조식' )) {
+	$sql1 = "UPDATE inquiry SET breakfast=(breakfast+1) ORDER BY id DESC LIMIT 1;";
 	$result=mysqli_query($conn, $sql1);
 	echo $result;
-	setcookie('breakfast', 1, time() + 86400,'/');
+	setcookie('breakfasttomorrow', 1, time() + 86400,'/');
 }
 
-if(($_COOKIE["lunch"]==0)&&(!empty($_POST['중식'])) && ( $_POST['중식'] == '중식' )) {
-	$sql2 = "UPDATE numeat SET num_eat=(num_eat+1) WHERE meal='lunch';";
+if(($_COOKIE["lunchtomorrow"]==0)&&(!empty($_POST['중식내일'])) && ( $_POST['중식내일'] == '중식' )) {
+	$sql2 = "UPDATE inquiry SET lunch=(lunch+1) ORDER BY id DESC LIMIT 1;";
 	$result=mysqli_query($conn, $sql2);
-	setcookie('lunch', 1, time() + 86400,'/');
+	setcookie('lunchtomorrow', 1, time() + 86400,'/');
 
 }
-if( ($_COOKIE["dinner"]==0)&&(!empty($_POST['석식'])) && ( $_POST['석식'] == '석식' )) {
-	$sql3 = "UPDATE numeat SET num_eat=(num_eat+1) WHERE meal='dinner';";
+if( ($_COOKIE["dinnertomorrow"]==0)&&(!empty($_POST['석식내일'])) && ( $_POST['석식내일'] == '석식' )) {
+	$sql3 = "UPDATE inquiry SET dinner=(dinner+1) ORDER BY id DESC LIMIT 1;";
 	$result=mysqli_query($conn, $sql3);
-	setcookie('dinner', 1, time() + 86400,'/');
+	setcookie('dinnertomorrow', 1, time() + 86400,'/');
 }
 
 //눌러져 있는 상태에서 다시 눌러서 취소할 때
-if( ($_COOKIE["breakfast"]==1)&&(!empty($_POST['조식'])) && ( $_POST['조식'] == '조식' )) {
-	$sql11 = "UPDATE numeat SET num_eat=(num_eat-1) WHERE meal='breakfast';";
+if( ($_COOKIE["breakfasttomorrow"]==1)&&(!empty($_POST['조식내일'])) && ( $_POST['조식내일'] == '조식' )) {
+	$sql11 = "UPDATE inquiry SET breakfast=(breakfast-1) ORDER BY id DESC LIMIT 1;";
 	$result=mysqli_query($conn, $sql11);
-	setcookie('breakfast', 0, time() + 86400,'/');
+	setcookie('breakfasttomorrow', 0, time() + 86400,'/');
 }
 
-if( ($_COOKIE["lunch"]==1)&&(!empty($_POST['중식'])) && ( $_POST['중식'] == '중식' )) {
-	$sql22 = "UPDATE numeat SET num_eat=(num_eat-1) WHERE meal='lunch';";
+if( ($_COOKIE["lunchtomorrow"]==1)&&(!empty($_POST['중식내일'])) && ( $_POST['중식내일'] == '중식' )) {
+	$sql22 = "UPDATE inquiry SET lunch=(lunch-1) ORDER BY id DESC LIMIT 1;";
 	$result=mysqli_query($conn, $sql22);
-	setcookie('lunch', 0, time() + 86400,'/');
+	setcookie('lunchtomorrow', 0, time() + 86400,'/');
 }
-if( ($_COOKIE["dinner"]==1)&&(!empty($_POST['석식'])) && ( $_POST['석식'] == '석식' )) {
-	$sql33 = "UPDATE numeat SET num_eat=(num_eat-1) WHERE meal='dinner';";
+if( ($_COOKIE["dinnertomorrow"]==1)&&(!empty($_POST['석식내일'])) && ( $_POST['석식내일'] == '석식' )) {
+	$sql33 = "UPDATE inquiry SET dinner=(dinner-1) ORDER BY id DESC LIMIT 1;";
 	$result=mysqli_query($conn, $sql33);
-	setcookie('dinner', 0, time() + 86400,'/');
+	setcookie('dinnertomorrow', 0, time() + 86400,'/');
 }
+
+///////////////////////////////////////////////////////////////
+
+if(($_COOKIE["breakfasttoday"]==0) and (!empty($_POST['조식오늘'])) and ( $_POST['조식오늘'] == '조식' )) {
+	$sql1 = "UPDATE inquiry SET breakfast=(breakfast+1) WHERE date='".date('y-m-d')."';";
+	$result=mysqli_query($conn, $sql1);
+	echo $result;
+	setcookie('breakfasttoday', 1, time() + 86400,'/');
+}
+
+if(($_COOKIE["lunchtoday"]==0)&&(!empty($_POST['중식오늘'])) && ( $_POST['중식오늘'] == '중식' )) {
+	$sql2 = "UPDATE inquiry SET lunch=(lunch+1) WHERE date='".date('y-m-d')."';";
+	$result=mysqli_query($conn, $sql2);
+	setcookie('lunchtoday', 1, time() + 86400,'/');
+
+}
+if( ($_COOKIE["dinnertoday"]==0)&&(!empty($_POST['석식오늘'])) && ( $_POST['석식오늘'] == '석식' )) {
+	$sql3 = "UPDATE inquiry SET dinner=(dinner+1) WHERE date='".date('y-m-d')."';";
+	$result=mysqli_query($conn, $sql3);
+	setcookie('dinnertoday', 1, time() + 86400,'/');
+}
+
+//눌러져 있는 상태에서 다시 눌러서 취소할 때
+if( ($_COOKIE["breakfasttoday"]==1)&&(!empty($_POST['조식오늘'])) && ( $_POST['조식오늘'] == '조식' )) {
+	$sql11 = "UPDATE inquiry SET breakfast=(breakfast-1) WHERE date='".date('y-m-d')."';";
+	$result=mysqli_query($conn, $sql11);
+	setcookie('breakfasttoday', 0, time() + 86400,'/');
+}
+
+if( ($_COOKIE["lunchtoday"]==1)&&(!empty($_POST['중식오늘'])) && ( $_POST['중식오늘'] == '중식' )) {
+	$sql22 ="UPDATE inquiry SET lunch=(lunch-1) WHERE date='".date('y-m-d')."';";
+	$result=mysqli_query($conn, $sql22);
+	setcookie('lunchtoday', 0, time() + 86400,'/');
+}
+if( ($_COOKIE["dinnertoday"]==1)&&(!empty($_POST['석식오늘'])) && ( $_POST['석식오늘'] == '석식' )) {
+	$sql33 ="UPDATE inquiry SET dinner=(dinner-1) WHERE date='".date('y-m-d')."';";
+	$result=mysqli_query($conn, $sql33);
+	setcookie('dinnertoday', 0, time() + 86400,'/');
+}
+
 if($result === false){
 	echo '<!DOCTYPE html>
 		<html lang="ko">
